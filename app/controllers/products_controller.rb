a59f9fdb137be33213e2bb93a8576d11169ca5b2
Reilly Bergin-Pugh <rberginpugh@gmail.com>
2017-05-11 21:23:07 -0400

    added entire REST code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,35 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render "index.html.erb"
+  end
+  def product_form_method
+    render "product_form.html.erb"
+  end
+  def create
+    @product = Product.new(name: params[:name_param], price: params[:price_param], description: params[:description_param])
+    @product.save
+    render "create.html.erb"
+  end
+  def show
+    @product = Product.find_by(id: params[:id])
+    render "show.html.erb"
+  end
+  def new
+    render "new.html.erb"
+  end
+  def edit
+    @product = Product.find_by(id: params[:id])
+    render "edit.html.erb"
+  end
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(name: params[:name_param], price: params[:price_param], description: params[:description_param])
+    render "update.html.erb"
+  end
+  def destroy
+    @product = Product.find_by(id: params[:id])
+    @product.destroy
+    render "destroy.html.erb"
+  end
 end
